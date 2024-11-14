35056a3a8a94b712f1535fb9d81017b0923ae41b
Nuno Reis <nunordreis@gmail.com>
2024-11-14 12:31:34 +0000

    test

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -3,17 +3,16 @@ package org.example;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-//teste 40381
-//test 44107
+
 public class Main {
     public static void main(String[] args) {
         // Replace these with your actual MySQL database details
         String url = "jdbc:mysql://localhost:3306/evaluationmap";
-        String username = "your_username";
+        String username2 = "your_username";
         String password = "your_password";
 
         // Attempt to connect to the database
-        try (Connection connection = DriverManager.getConnection(url, username, password)) {
+        try (Connection connection = DriverManager.getConnection(url, username2, password)) {
             if (connection != null) {
                 System.out.println("Connected to the database successfully!");
             } else {
